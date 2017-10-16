b66f3bf2f80253e29b5cc8d42b8d512435b848ee
fares abadou <fares.abadou@gmail.com>
2017-10-17 00:31:10 +0100

    added yellow main section

--- home.php
+++ home.php
@@ -7,27 +7,7 @@
     <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Amatic+SC:700" rel="stylesheet">
 
-<style media="screen">
-.navbar-dark .navbar-toggler {
-color: #000 !important;
-border-color: #FFEA00 !important;
-}
-.navbar-dark .navbar-brand {
-  color:#FFEA00;
-  font-family: 'Amatic SC', cursive;
-font-size: 32px;
-line-height: 30px;
-font-weight: bold;
-}
-
-
-.navbar-dark .navbar-nav .active>.nav-link, .navbar-dark .navbar-nav .nav-link, .navbar-dark .navbar-nav .nav-link.show, .navbar-dark .navbar-nav .show>.nav-link {
-
-  color:#FFEA00;
-}
-
-
-</style>
+<link rel="stylesheet" href="libs/main/css/main.css">
     <!-- Bootstrap CSS -->
   </head>
   <body>
@@ -36,7 +16,7 @@ font-weight: bold;
 
 
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark nav-top fixed-top">
       <a class="navbar-brand" href="#">Khadamet</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -55,8 +35,8 @@ font-weight: bold;
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+          <input class="form-control mr-sm-2" type="text" placeholder="Recherche" aria-label="Search">
+          <button class="btn btn-warning my-2 my-sm-0" type="submit">Recherche</button>
         </form>
       </div>
     </nav>
@@ -66,7 +46,44 @@ font-weight: bold;
 
 
 
+<section class="main">
 
+</section>
+
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 
 
     <h1>Hello, world!</h1>
@@ -76,5 +93,18 @@ font-weight: bold;
     <script src="libs/bootstrap4/js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="libs/bootstrap4/js/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="libs/bootstrap4/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+function testScroll(ev){
+  var winH = window.innerHeight	;
+  if(window.pageYOffset> (winH - 200)) {document.querySelector('nav').classList.remove('nav-top');
+}else {
+  document.querySelector('nav').classList.add('nav-top');
+}
+}
+testScroll();
+window.onscroll=testScroll;
+</script>
+
   </body>
 </html>
