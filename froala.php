c1728f2377f556de4ed56b7a78d6f6c29cdcc1c8
Fares ABADOU <fares.abadou@gmail.com>
2017-10-18 01:37:30 +0100

    added froala

---
+++ froala.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Amatic+SC:700" rel="stylesheet">
+
+<link rel="stylesheet" href="libs/main/css/main.css">
+<link type="text/css" rel="stylesheet" href="libs/froala/css/froala_blocks.css">
+<link type="text/css" rel="stylesheet" href="libs/font-awesome/css/font-awesome.css">
+
+    <!-- Bootstrap CSS -->
+  </head>
+  <body>
+
+
+
+
+
+
+
+          <nav class="navbar navbar-expand-lg  nav-top fixed-top ">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav10" aria-controls="navbarNav10" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="col-4 col-sm-2 text-center order-lg-6">
+              <a href="https://www.froala.com">
+                <img src="libs/froala/imgs/img_logo.png" height="30" alt="image">
+              </a>
+            </div>
+
+            <div class="collapse navbar-collapse col-12 col-md-5 order-lg-1" id="navbarNav10">
+              <ul class="navbar-nav col-5">
+                <li class="nav-item active">
+                  <a class="nav-link" href="https://www.froala.com">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.froala.com">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.froala.com">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.froala.com">Team</a>
+                </li>
+              </ul>
+            </div>
+
+            <ul class="navbar-nav justify-content-end col-sm-5 order-lg-12 d-none d-md-flex">
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.froala.com"><i class="fa fa-twitter"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.froala.com"><i class="fa fa-youtube"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.froala.com"><i class="fa fa-facebook"></i></a>
+              </li>
+            </ul>
+          </nav>
+
+
+
+
+
+
+
+    <section class="fdb-block fdb-viewport bg-dark" style="background-image: url(libs/froala/imgs/bg_0.svg)">
+        <div class="container justify-content-center align-items-center d-flex">
+          <div class="row justify-content-center text-center">
+            <div class="col-12 col-md-8">
+              <img alt="image" class="fdb-icon" src="libs/froala/imgs/img_round.svg">
+              <h1>Froala Design Blocks</h1>
+              <p class="text-h3">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+              <p class="mt-5"><a href="https://www.froala.com" class="btn">Call to Action</a></p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+
+
+
+
+
+      <section class="fdb-block pb-0">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-10 col-sm-6 col-md-5 col-lg-4 m-auto pb-5 pb-md-0">
+                <img alt="image" class="img-fluid br-0" src="libs/froala/imgs/img_tall.png">
+              </div>
+
+              <div class="col-12 ml-md-auto col-md-7 col-lg-5 pb-5 pb-md-0">
+                <img alt="image" class="fdb-icon" src="libs/froala/imgs/img_square_1.svg">
+                <h1>Design Blocks</h1>
+                <p class="text-h3">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts</p>
+                <p class="mt-4"><a class="btn" href="https://www.froala.com">Download</a><a class="btn" href="https://www.froala.com">Register</a></p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+
+        <section class="fdb-block fdb-image-bg" style="background: url(libs/froala/imgs/img_bg.jpg)">
+            <div class="container">
+              <div class="row text-center">
+                <div class="col-12">
+                  <h1>Les sercives</h1>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+          <section  class="fdb-block">
+
+<div class="container">
+
+          <div class="row">
+                  <div class="col-sm-3 text-left">
+                    <div class="fdb-box">
+                      <img alt="image" class="img-fluid" src="libs/froala/imgs/img_square_1.svg">
+
+                      <div class="content">
+                        <h3><strong>Sara Doe</strong></h3>
+                        <p>Founder</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-sm-3 text-left">
+                    <div class="fdb-box">
+                      <img alt="image" class="img-fluid" src="libs/froala/imgs/img_square_2.svg">
+
+                      <div class="content">
+                        <h3><strong>Sara Doe</strong></h3>
+                        <p>Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-3 text-left">
+                    <div class="fdb-box">
+                      <img alt="image" class="img-fluid" src="libs/froala/imgs/img_square_3.svg">
+
+                      <div class="content">
+                        <h3><strong>Sara Doe</strong></h3>
+                        <p>Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-3 text-left">
+                    <div class="fdb-box">
+                      <img alt="image" class="img-fluid" src="libs/froala/imgs/img_square_4.svg">
+
+                      <div class="content">
+                        <h3><strong>Sara Doe</strong></h3>
+                        <p>Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+</section>
+
+
+
+
+  <section class="fdb-block">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-sm-10 col-md-8 col-lg-4 text-center">
+            <p class="text-h3">
+              Follow us and ask more on:
+            </p>
+            <p class="text-h2">
+              <a href="https://www.froala.com"><i class="fa fa-facebook"></i></a>&nbsp;&nbsp;&nbsp;
+              <a href="https://www.froala.com"><i class="fa fa-twitter"></i></a>&nbsp;&nbsp;&nbsp;
+              <a href="https://www.froala.com"><i class="fa fa-instagram"></i></a>&nbsp;&nbsp;&nbsp;
+              <a href="https://www.froala.com"><i class="fa fa-google"></i></a>&nbsp;&nbsp;&nbsp;
+              <a href="https://www.froala.com"><i class="fa fa-pinterest"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+    <footer class="fdb-block footer-small bg-dark">
+        <div class="container">
+          <div class="row text-center align-items-center">
+            <div class="col">
+              <ul class="nav justify-content-center">
+                <li class="nav-item">
+                  <a class="nav-link active" href="https://www.froala.com">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.froala.com">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.froala.com">Privacy Policy</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.froala.com">Terms</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.froala.com">About</a>
+                </li>
+              </ul>
+
+              <p class="text-h5 mt-5">© 2013-2017 Froala</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="libs/bootstrap4/js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="libs/bootstrap4/js/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="libs/bootstrap4/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+function testScroll(ev){
+  var winH = window.innerHeight	;
+  if(window.pageYOffset> (winH - 200)) {document.querySelector('nav').classList.remove('nav-top');
+}else {
+  document.querySelector('nav').classList.add('nav-top');
+}
+}
+testScroll();
+window.onscroll=testScroll;
+</script>
+
+  </body>
+</html>
