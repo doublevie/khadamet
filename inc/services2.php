3b045936515452df30e582e250445db8299a951f
Fares ABADOU <fares.abadou@gmail.com>
2017-11-09 00:52:13 +0100

    added photos and activities

--- inc/services2.php
+++ inc/services2.php
@@ -3,10 +3,36 @@
 <div class="container-fluid">
 
 <div class="row">
+
+
         <div class="col-md-3 col-sm-6 text-left">
           <div class="fdb-box serv" style="background:url(img/s/plombier.jpg)">
-       <div class="ttl">
+       <div class="ttl ca">
         PLOBMIER
+       </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6 text-left">
+          <div class="fdb-box serv" style="background:url(img/s/electricien.jpg)">
+       <div class="ttl ca">
+        ELECTRICIEN
+       </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6 text-left">
+          <div class="fdb-box serv" style="background:url(img/s/painter.jpg)">
+       <div class="ttl ca">
+        PAINTER
+       </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6 text-left">
+          <div class="fdb-box serv" style="background:url(img/s/macon.jpg)">
+       <div class="ttl ca">
+        MAÇON
        </div>
           </div>
         </div>
--- index.php
+++ index.php
@@ -83,7 +83,7 @@
             <div class="container">
               <div class="row text-center">
                 <div class="col-12">
-                  <h1 style="color:#FFFF00">Les sercives</h1>
+                  <h1 style="color:#FFFF00" class="ca">Les sercives</h1>
                 </div>
               </div>
             </div>
